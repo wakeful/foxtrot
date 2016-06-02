84098744ddd8ff13a7eb91974b574214243fcfbc
AJ <aj@laatu.uk>
2016-06-02 19:34:48 +0100

    added base scripts

---
+++ Vagrantfile
@@ -0,0 +1,30 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure(2) do |config|
+
+  config.vm.box = "debian/contrib-jessie64"
+
+  config.vm.hostname = "foxtrot"
+
+  config.vm.provider "virtualbox" do |vb|
+
+    vb.gui = true
+
+    vb.cpus = 2
+    vb.memory = "2048"
+
+  end
+
+  config.vm.provision :shell, :path => "https://raw.githubusercontent.com/wakeful/puppet-bootstrap/master/deb.sh"
+
+  config.vm.provision :puppet do |puppet|
+
+    puppet.environment = "dev"
+    puppet.environment_path = "puppet"
+
+    puppet.options = "--verbose --debug"
+
+  end
+
+end
---
+++ puppet/dev/modules/baseconf/manifests/init.pp
@@ -0,0 +1,42 @@
+class baseconf {
+
+  package { ['apt-transport-https', 'ca-certificates', 'git', 'mutt', 'python-setuptools', 'tmux', 'vim']:
+    ensure => present
+  }
+
+  file { '/etc/motd':
+    backup => false,
+    content => template('baseconf/motd.erb'),
+    ensure => file
+  }
+
+  Exec { 
+    path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+  }
+
+  exec { 'install docker':
+    command => 'curl -sSL https://get.docker.com/ | sh',
+    unless => 'which docker'
+  }
+
+  service { 'docker':
+    enable => true,
+    ensure => running
+  }
+
+  user { 'vagrant':
+    ensure => 'present',
+    groups => ['docker']
+  }
+
+  exec { 'install pip':
+    command => 'easy_install pip',
+    unless => 'which pip'
+  }
+
+  exec { 'install docker-compose': 
+    command => 'pip install docker-compose',
+    unless => 'which docker-compose'
+  }
+
+}
